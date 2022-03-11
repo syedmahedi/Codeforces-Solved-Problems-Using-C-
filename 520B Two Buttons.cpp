83e7008814b2f8609940a9cc750ad776780ac166
Syed Mahedi Hasen <syedmahedihasen207@gmail.com>
2022-03-11 11:28:04 +0600

    Add files via upload

---
+++ 520B Two Buttons.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+    #define   ll    long long
+    #define   pb    push_back
+    #define   endl  "\n"
+    #define   mp    make_pair
+    #define   yes   cout<<"YES\n"
+    #define   no    cout<<"NO\n"
+    #define   tst   int t;cin>>t;
+
+//check
+    bool   isPrime(ll n){if(n<=1)return false;
+           if(n<=3)return true;if(n%2==0|n%3==0)return false;
+           for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
+
+//mathmatics funtion
+    ll     gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);}
+    ll     lcm(ll a, ll b){return (a/gcd(a,b)*b);}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n,m,cnt=0;
+    cin>>n>>m;
+    while(m>n)
+    {
+        if(m%2==0)
+        {
+            m/=2;
+        }
+        else
+            m++;
+        cnt++;
+
+    }
+    cnt+=(n-m);
+
+
+    cout<<cnt<<endl;
+
+
+
+
+    return 0;
+}
