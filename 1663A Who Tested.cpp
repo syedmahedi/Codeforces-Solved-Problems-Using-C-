a2054df25441c770d984d28071e1f5f0653e6c83
Syed Mahedi Hasen <syedmahedihasen207@gmail.com>
2022-04-02 09:58:09 +0600

    Add files via upload

---
+++ 1663A Who Tested.cpp
@@ -0,0 +1,16 @@
+    #include <iostream>
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    int main()
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+        string s;
+        cin>>s;
+        cout<<"Red Panda"<<endl;
+
+
+        return 0;
+    }
---
+++ 1663C Pōja Verdon.cpp
@@ -0,0 +1,24 @@
+    #include <iostream>
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    int main()
+    {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+
+        int n;
+        cin>>n;
+        int a[n];
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+            sum+=a[i];
+        }
+        cout<<sum<<endl;
+
+
+
+        return 0;
+    }
